aa49a81e8987b2e803e07a37cba723fabc912ee2
gar212 <gartoyou@gmail.com>
2021-05-25 17:01:10 +1200

    complete with error handling

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
 const express = require('express');
+var cors = require('cors');
 const fileUpload = require('express-fileupload');
 const AWS = require('aws-sdk');
 
 const app = express();  
-const port = 3000;
+const port = 5000;
 
 //Middleware
+app.use(cors({
+  origin: true,
+  methods:["GET", "POST"],
+  credentials: true
+}));
+
 app.use(fileUpload());
 
 AWS.config.loadFromPath('./config/credentials.json');
 
 const rekognition = new AWS.Rekognition(); 
 
-/* This operation detects labels in the supplied image */
-
 const detectImage = (image, callback) => {
   var params = {
-    Image: {
-     Bytes: image.data.buffer
-    }, 
-    MaxLabels: 20, 
+    Image: { 
+      Bytes: Buffer.from(image.file.data),
+    },
+    ProjectVersionArn: "arn:aws:rekognition:ap-southeast-2:113539865244:project/Amazon-Rekognition-Project/version/Amazon-Rekognition-Project.2021-05-24T15.42.53/1621827827086",
+    MaxResults: 15,
     MinConfidence: 70
-   };
-   rekognition.detectLabels(params, function(err, data) {
-     if (err) console.log(err, err.stack); // an error occurred
-     else {
-       const mapResult = data.Labels.map(function (el){
-         return el.Name;
-       })
-       callback(mapResult);
-    } // Successful
-   });
+  };
+  rekognition.detectCustomLabels(params, function(err, data) {
+      if (err){ // an error occurred
+        callback(err);
+        console.log(err);
+      } 
+      else { // Successful
+        callback(data.CustomLabels);
+      } 
+  });
 }
 
-   app.use(express.static('public'))
+  //  app.use(express.static('public'))
 
    // Controller
    app.post('/upload', function(req, res) {
-
-    let sampleFile;
-    let uploadPath;
+    let image;
   
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
   
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    sampleFile = req.files.sampleFile;
-    uploadPath = __dirname + sampleFile.name;
-    detectImage(sampleFile, function(response){
-      console.log(response);
-      res.send(response);
+    image = req.files;
+    detectImage(image, function(response){
+      res.status(200).send(response);
     })
   });
 
